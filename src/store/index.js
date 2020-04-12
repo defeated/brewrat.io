2e37f9bb487d1e3acb9ae0cfcf17448b4d6ef639
eddie cianci <defeated2k4@gmail.com>
2020-04-12 11:44:24 -0400

    add sample Drink data, add to Store

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    drinks: []
   },
+
   mutations: {
+    SET_DRINKS(state, drinks) {
+      state.drinks = drinks
+    }
   },
+
   actions: {
+    async fetchDrinks({ commit }) {
+      let response = await fetch('/drinks.json')
+      let data = await response.json()
+      commit('SET_DRINKS', data.drinks)
+    }
   },
+
   modules: {
   }
 })
