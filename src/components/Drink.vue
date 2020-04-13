aa25f32beefa78c2e527da274286e818781bd7f0
eddie cianci <defeated2k4@gmail.com>
2020-04-12 20:30:21 -0400

    initial drink grid
    explored data binding, vuex store, fetch api, scoped styles, tag groups)

---
+++ src/components/Drink.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card tile>
+    <v-img :src="drink.photo" height="240" class="white--text align-end">
+      <v-card-title class="py-1">
+        {{ drink.name }}
+      </v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="text-truncate">
+      {{ drink.subtitle }}
+    </v-card-subtitle>
+
+    <v-card-text>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est distinctio numquam sequi cumque, odio eaque facilis impedit veritatis reprehenderit id inventore laboriosam, esse consectetur repudiandae? Inventore eum omnis sapiente odit?
+
+      <v-chip-group class="mt-3">
+        <v-chip tile v-for="tag in drink.tags" :key="tag">
+          <v-icon left>
+            {{ tagIcons[tag] }}
+          </v-icon>
+          {{ tag }}
+        </v-chip>
+      </v-chip-group>
+
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['drink'],
+
+  data() {
+    return {
+      tagIcons: {
+        cold: "mdi-snowflake",
+        hot: "mdi-fire",
+        espresso: "mdi-coffee",
+        pourover: "mdi-water",
+        milk: "mdi-glass-pint-outline",
+        dessert: "mdi-ice-cream",
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-card__title {
+  background: rgba(0, 0, 0, 0.25);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,40 @@
 <template>
-  <div class="home">
-    Home
-  </div>
+  <v-container>
+
+    <v-data-iterator :items="drinks" :items-per-page="-1" hide-default-footer loading loading-text="Brewing...">
+      <template v-slot:default="props">
+        <v-row>
+          <v-col cols="12" md="4" v-for="item in props.items" :key="item.id">
+            <Drink :drink="item" />
+          </v-col>
+        </v-row>
+      </template>
+    </v-data-iterator>
+
+  </v-container>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import Drink from '@/components/Drink.vue'
+
+export default {
+  components: {
+    Drink
+  },
+
+  computed: {
+    ...mapState(['drinks'])
+  },
+
+  mounted() {
+    this.$store.dispatch('fetchDrinks')
+  }
+}
 </script>
+
+<style scoped>
+.v-card__title {
+  background: rgba(0, 0, 0, 0.25);
+}
+</style>
